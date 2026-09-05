cd7477a9293fb0aabf58782c96c1063cae8385b9
Heng David <hengdavid129@gmail.com>
2026-09-05 10:22:22 +0700

    Handle a Form Submission

---
+++ Event Handling/Episode 8/js/components/AssignmentList.js
@@ -0,0 +1,26 @@
+import Assignment from "./Assignment.js";
+
+export default {
+    components: {
+        Assignment
+    },
+
+    template: `
+        <section v-show="assignments.length">
+            <h1 class="font-bold">{{ title }}</h1>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600">
+                <assignment
+                    v-for="assignment in assignments"
+                    :key="assignment.id"
+                    :assignment="assignment"
+                ></assignment>
+            </ul>
+        </section>
+    `,
+
+    props: {
+        assignments: Array,
+        title: String
+    }
+}
